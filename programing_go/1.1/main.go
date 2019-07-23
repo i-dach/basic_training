65d329ae7a13df97804f48067ae082e502f41eb1
iizuka.daichi <iizuka.daichi@designone.jp>
2019-07-23 09:18:17 +0900

    update: practice 1

--- programing_go/1.1/main.go
+++ programing_go/1.1/main.go
@@ -1,5 +1,15 @@
-package p1
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
 
 func practice1() {
+	fmt.Println(strings.Join(os.Args[0:], " "))
+}
 
+func main() {
+	practice1()
 }
--- programing_go/1.1/main_test.go
+++ programing_go/1.1/main_test.go
@@ -1,16 +1,22 @@
-package p1
+package main
 
 import "testing"
 
 func Test_practice1(t *testing.T) {
+	// 前準備
 	tests := []struct {
 		name string
 	}{
 		// TODO: Add test cases.
 	}
+
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			// 実行
 			practice1()
+
+			// 検証
+
 		})
 	}
 }
